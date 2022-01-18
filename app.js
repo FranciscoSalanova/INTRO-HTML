c7bec7ec7931bfaad66d0ac00e9e4280a2317460
Francisco Salanova <fsalanova.16@gmail.com>
2022-01-18 17:58:31 -0300

    Agregado de nuevaPagina y diseño adaptativo a la pagina

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 
 const publicPath = path.resolve(__dirname, './public');
+
 app.use(express.static(publicPath));
 
 app.listen(3000, () => {
@@ -12,4 +13,8 @@ app.listen(3000, () => {
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.get('/nuevaPagina', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/nuevaPagina.html'));
 });
